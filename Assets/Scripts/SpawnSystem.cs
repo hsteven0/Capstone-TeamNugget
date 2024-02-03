ef26f9ff01d0bcb98e11a87abc8e2453b99a650c
Steven Hua <steven.hua989@outlook.com>
2024-02-02 22:31:16 -0700

    Implemented time based random spawning

---
+++ Assets/Scripts/SpawnSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSystem : MonoBehaviour
+{
+    public GameObject[] enemies;
+    private float timer = 0.0f;
+    private float spawnDelay = 2.0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // if time has reached spawn delay time
+        if (timer >= spawnDelay) {
+            Vector3 randPos = new Vector3(Random.Range(-23, 24), 16, 0);
+            Instantiate(enemies[Random.Range(0, enemies.Length)], randPos, Quaternion.identity);
+
+            // subtract spawnDelay time from total time; this is more accurate over time
+            timer -= spawnDelay;
+        }
+    }
+}
