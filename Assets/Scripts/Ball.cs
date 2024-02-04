a38a33fd6e7e2cfd5d8740c91524b0b858cb67c0
Steven Hua <steven.hua989@outlook.com>
2024-02-03 19:49:17 -0700

    Set up base for point multiplyer for single ball multikills

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private int killCount; // a count of enemies defeated with one ball
+
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+        killCount = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        killCount++;
+        UpdatePoints();
+        // Debug.Log(killCount);
+    }
+
+    private void UpdatePoints() {
+        // reward player with more points, based on number of enemies defeated with a single ball
+    }
+}
