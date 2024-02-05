7fc86afba422866db92d644961aede1f3112ebcc
Steven Hua <steven.hua989@outlook.com>
2024-02-04 23:45:07 -0700

    Implemented points text to change colours dynamically

---
+++ Assets/Scripts/Animations/PointsText.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PointsText : MonoBehaviour
+{
+    public TextMeshPro pointsText;
+
+    void Start()
+    {
+        // this changes to red
+        // pointsText.faceColor = new Color32(255, 0, 0, 255);
+    }
+}
