432fab64f0b7d7ff7080afcc1bef0877342424dd
Steven Hua <steven.hua989@outlook.com>
2024-02-21 15:10:41 -0700

    Implemented scaling to the Main Menu

---
+++ Assets/Scenes/MainMenu/GradientControl.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/*
+This class controls the gradient of all TextMeshProUI's in the Canvas GameObject
+using Lerp(). The colors fade up and down, giving a "ping pong" effect to it.
+*/
+public class GradientControl : MonoBehaviour
+{
+    private Color colorTop;
+    private Color colorBottom;
+    private float fadeDuration = 1.2f, fadeStart = 0.0f;
+    private bool reachedBottom;
+
+    void Start() {
+        colorTop = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+        colorBottom = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        reachedBottom = false;
+    }
+
+    void Update()
+    {
+        var textComponents = gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+        // slowly reduce values over time
+        if (fadeStart < fadeDuration) {
+            fadeStart += Time.deltaTime / fadeDuration;
+            foreach (TextMeshProUGUI text in textComponents) {
+                if (text.name == "PlayText") continue;
+                // color calculations
+                var colTop = Color.Lerp(colorTop, colorBottom, fadeStart);
+                var colBot = Color.Lerp(colorBottom, colorTop, fadeStart);
+                
+                if (!reachedBottom) {
+                    text.colorGradient = new VertexGradient(colTop, colTop, colBot, colBot);
+                } else {
+                    text.colorGradient = new VertexGradient(colBot, colBot, colTop, colTop);
+                }
+            }
+        } else {
+            reachedBottom = !reachedBottom;
+            fadeStart = 0;
+        }
+    }
+}
