3ee6e11e45de7b574f715e3e9dfde20d4bd3248a
Steven Hua <steven.hua989@outlook.com>
2024-02-23 00:33:47 -0700

    Added visuals to the Lives System on the Game Scene

--- Assets/Scripts/LivesSystem.cs
+++ Assets/Scripts/LivesSystem.cs
@@ -10,18 +10,36 @@ public class LivesSystem : MonoBehaviour
     private float prevTime;
     private float idleTime;
     public static int lives;
+    public Image[] hearts;
+    public Sprite fullHeart, emptyHeart;
+    private bool updateHearts;
 
     void Start()
     {
         prevTime = Time.timeScale;
+        updateHearts = true;
         lives = 3;
     }
 
     void Update()
     {
-        // "<" incase multiple enemies die at the same time when lives is 1
-        if (lives <= 0) {
+        if (updateHearts)
+        {
+            foreach (Image i in hearts)
+            {
+                i.sprite = emptyHeart;
+            }
+            for (int i = 0; i < lives; i++)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+        }
+
+        if (lives <= 0)
+        {
+            // "<" incase multiple enemies die at the same time when lives is 1
             GameOver();
+            updateHearts = false; 
         }
     }
 
