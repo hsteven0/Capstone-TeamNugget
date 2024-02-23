944ebe8fa1b3a83782f176b476b9eed9d6e0e14a
Steven Hua <steven.hua989@outlook.com>
2024-02-23 12:09:00 -0700

    Fixed an issue with the point multiplier not updating properly

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,21 +1,23 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 /*
 This class will be used as calculating point multipliers upon contact with enemies.
 */
 public class Ball : MonoBehaviour
 {
-    private int killCount = 0; // a count of enemies defeated with one ball
+    private int killCount; // a count of enemies defeated with one ball
     private int oldAmount;
+    public GameObject pointsText;
 
     void Start() {
+        killCount = 0;
         oldAmount = GameScore.points;
     }
 
+    // gets every object that is destroyed on the scene
     void OnDestroy() {
+        // check only for SlingItem tag
         if (gameObject.tag != "SlingItem") return;
         killCount = 0;
         GameScore.points = oldAmount;
@@ -30,6 +32,9 @@ public class Ball : MonoBehaviour
 
     private void UpdatePoints() {
         // reward player with more points, based on number of enemies defeated with a single ball
-        GameScore.points = oldAmount * (int) Math.Pow(2, killCount);
+        GameScore.points = oldAmount * (int) Mathf.Pow(2, killCount);
+        GameScore.score += GameScore.points;
+        GameObject point = Instantiate(pointsText, transform.position, Quaternion.identity);
+        point.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(GameScore.points.ToString());
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,23 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject deathEffect, pointsText;
+    public GameObject deathEffect;
 
     void OnCollisionEnter2D(Collision2D collInfo) {
-        // less laggy option to ignore enemy colliding with another enemy
-        // need to optimize
-        if(collInfo.collider.gameObject.layer == 7) return;
-        // reward or punish player(s)
-        if (collInfo.collider.tag == "SlingItem") {
-            Score();
-        }
-        else if (collInfo.collider.tag == "Ground") {
+        if (collInfo.collider.tag == "Ground") {
             LivesSystem.lives--;
         }
         Die();
@@ -25,13 +13,7 @@ public class Enemy : MonoBehaviour
 
     void Die() {
         GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 1.5f);
+        Destroy(effect, 0.5f);
         Destroy(gameObject);
-    }
-
-    private void Score() {
-        GameScore.score += GameScore.points;
-        GameObject point = Instantiate(pointsText, transform.position, Quaternion.identity);
-        point.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(GameScore.points.ToString());
     }
 }
