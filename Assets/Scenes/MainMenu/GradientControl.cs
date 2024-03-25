061ee3c2224922d3d5d2c179fe86c6f42deb645b
Steven Hua <steven.hua989@outlook.com>
2024-03-25 00:48:23 -0600

    Fonts and other main menu changes

--- Assets/Scenes/MainMenu/GradientControl.cs
+++ Assets/Scenes/MainMenu/GradientControl.cs
@@ -14,34 +14,63 @@ public class GradientControl : MonoBehaviour
     private Color colorBottom;
     private float fadeDuration = 1.2f, fadeStart = 0.0f;
     private bool reachedBottom;
+    private float timer, hueTimer = 0.1F, fadingHue = 1F;
+    private bool hueDarkening;
+    private TextMeshProUGUI[] textComponents;
 
     void Start() {
-        colorTop = new Color(0.3f, 0.3f, 1.0f, 1.0f);
+        textComponents = gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+        colorTop = new Color(0.0f, 0.0f, 1.0f, 1.0f);
         colorBottom = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         reachedBottom = false;
+        timer = 0.0f;
     }
 
     void Update()
     {
-        var textComponents = gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+        timer += Time.deltaTime;
         // slowly reduce values over time
-        if (fadeStart < fadeDuration) {
-            fadeStart += Time.deltaTime / fadeDuration;
-            foreach (TextMeshProUGUI text in textComponents) {
-                if (text.GetComponentInParent<Button>()) continue;
-                // color calculations
-                var colTop = Color.Lerp(colorTop, colorBottom, fadeStart);
-                var colBot = Color.Lerp(colorBottom, colorTop, fadeStart);
+        // if (fadeStart < fadeDuration) {
+        //     fadeStart += Time.deltaTime / fadeDuration;
+        //     foreach (TextMeshProUGUI text in textComponents) {
+        //         if (text.GetComponentInParent<Button>()) continue;
+        //         // color calculations
+        //         var colTop = Color.Lerp(colorTop, colorBottom, fadeStart);
+        //         var colBot = Color.Lerp(colorBottom, colorTop, fadeStart);
                 
-                if (!reachedBottom) {
-                    text.colorGradient = new VertexGradient(colTop, colTop, colBot, colBot);
+        //         if (!reachedBottom) {
+        //             text.colorGradient = new VertexGradient(colTop, colTop, colBot, colBot);
+        //         } else {
+        //             text.colorGradient = new VertexGradient(colBot, colBot, colTop, colTop);
+        //         }
+        //     }
+        // } else {
+        //     reachedBottom = !reachedBottom;
+        //     fadeStart = 0;
+        // }
+        if (timer >= hueTimer) {
+            if (!hueDarkening) {
+                if (fadingHue > 0.6) {
+                    fadingHue -= 0.1F;
                 } else {
-                    text.colorGradient = new VertexGradient(colBot, colBot, colTop, colTop);
+                    hueDarkening = true;
+                }
+            } else {
+                if (fadingHue < 1) {
+                    fadingHue += 0.1F;
+                } else {
+                    hueDarkening = false;
                 }
             }
-        } else {
-            reachedBottom = !reachedBottom;
-            fadeStart = 0;
+            timer -= hueTimer;
         }
+        foreach (TextMeshProUGUI text in textComponents) {
+            if (text.GetComponentInParent<Button>()) continue;
+            text.faceColor = FadingBlue();
+        }
+    }
+
+    private Color FadingBlue() {
+        return Color.HSVToRGB(0.6F, fadingHue, 0.82f);
     }
 }
